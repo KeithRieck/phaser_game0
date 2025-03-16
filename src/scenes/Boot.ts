3c43e67d8ace03747158c8e8e08066fca661b17e
Keith Rieck <KeithRieck@gmail.com>
2025-03-16 11:17:05 -0500

    Add developer build script.  Clean up some console errors.

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,14 +1,19 @@
 import { Scene } from 'phaser';
 
+/**
+ * Boot minimal assets to support the Preloader Scene.
+ * The Boot Scene is typically used to load in any assets you require for your 
+ * Preloader, such as a game logo or background.
+ * The smaller the file size of the assets, the better, as the Boot Scene 
+ * itself has no preloader.
+ * 
+ */
 export class Boot extends Scene {
     constructor() {
         super('Boot');
     }
 
     preload(): void {
-        //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
-        //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
-
         this.load.setPath('assets');
         this.load.image('logo', 'logo.png');
     }
